b34a3f8b54bc520dda9212c6f497516ce1c2b019
Abhilash D G <dgabhi12@gmail.com>
2021-09-30 18:41:09 +0530

    FIRST-WEBAPP-F

---
+++ Demo/sample/cuisine/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+     # Cuisine_list view as a function
+     url(r'^$', views.CuisineListView.as_view(), name='cuisine_list_cls'),
+
+     #url(r'^$',
+     #views.Cuisine_list,
+     #name='Cuisine_list'
+     #),
+     # Cuisine_detail view as a function
+     url(r'^(?P<cuisine>[-\w]+)/$',
+     views.Cuisine_detail,
+     name='Cuisine_detail'
+     ),
+    ]
